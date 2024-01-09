bf7abe7c2ee7ea0575236de2089b80511d53ac45
Vikas Pal <154115836+vikas1pal@users.noreply.github.com>
2024-01-09 19:10:50 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "abcd"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "royal"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "RYL"
 )
 
 var ID ids.ID
